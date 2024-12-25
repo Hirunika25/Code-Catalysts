a3b4c9ead05cf00a75bc6044f1e24efdf9e37ae6
Hirunika SuwandaArachchi <nethmihirunika425@gmail.com>
2024-12-25 17:36:01 +0530

    update

--- EventAura/src/Pages/CommonPages/DeleteEvent/DeleteEvent.jsx
+++ EventAura/src/Pages/CommonPages/DeleteEvent/DeleteEvent.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-{/*import React from 'react'
-import { Link } from 'react-router-dom';
-import '../ViewEvent/ViewEvent.scss';
-import '../ViewEvent/ViewEvent.jsx';
-import './DeleteEvent.scss';*/}
 
 const DeleteEvent = () => {
   return (
-    <div>DeleteEvent</div>
+   <>
+   </>
   )
 }
 
