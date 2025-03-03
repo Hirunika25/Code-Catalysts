8206adb2e44f07037e595fd3a401f1b03aaffff5
Hirunika SuwandaArachchi <nethmihirunika425@gmail.com>
2025-03-03 08:53:04 +0530

    update Admin Home

--- EventAura/src/Pages/CommonPages/UserHome/UserHome.jsx
+++ EventAura/src/Pages/CommonPages/UserHome/UserHome.jsx
@@ -6,11 +6,12 @@ import AdminFooter from './../../../Components/Footer/AdminFooter';
 import './UserHome.scss';
 import { Link } from 'react-router-dom';
 
-{/*import Cover01 from '../../../assets/coverImage01.png';
-import Cover02 from '../../../assets/coverImage05.png';
-import Cover03 from '../../../assets/coverImage02.png';
-import Cover04 from '../../../assets/coverImage03.png';
-import Cover05 from '../../../assets/coverImage04.png';*/}
+import Cover01 from '../../../assets/Tile 1.jpg';
+import Cover02 from '../../../assets/Tile 2.jpg';
+import Cover03 from '../../../assets/Tile 3.jpg';
+import Cover04 from '../../../assets/Tile 4.jpg';
+import Cover05 from '../../../assets/Tile 5.jpg';
+import Cover06 from '../../../assets/Tile 6.jpg';
 
 
 
@@ -23,10 +24,11 @@ const UserHome = () => {
 
        
         <div className="row pt-5 px-3 gx-5">  
+          
               <div className="col-md-4 mb-4 position-relative"> 
                 <div className="cards shadow border-0">
                   <div className="cards-img-top">
-                   
+                      <img src={Cover01} alt="Banner1" className="w-100"/>
                       <Link to="/AdminOngoing" className="image-overlay">Ongoing Events</Link>
                   </div>
                 </div>
@@ -35,7 +37,7 @@ const UserHome = () => {
               <div className="col-md-4 mb-4 position-relative">
                 <div className="cards shadow border-0">
                   <div className="cards-img-top">
-                    
+                    <img src={Cover02} alt="Banner2" className="w-100"/>
                     <Link to="/" className="image-overlay">Pending Requests</Link>
                   </div>
               </div>
@@ -44,7 +46,7 @@ const UserHome = () => {
               <div className="col-md-4 mb-4 position-relative">
                 <div className="cards shadow border-0">
                   <div className="cards-img-top">
-                      
+                      <img src={Cover03} alt="Banner3" className="w-100"/>
                       <Link to="/" className="image-overlay">History</Link>
                   </div>
                 </div>
@@ -55,7 +57,7 @@ const UserHome = () => {
                 <div className="col-md-4 mb-4 position-relative"> 
                     <div className="cards shadow border-0">
                         <div className="cards-img-top">
-                            
+                            <img src={Cover04} alt="Banner4" className="w-100"/>
                             <Link to="/UserDetails" className="image-overlay">User Details</Link>
                         </div>
                     </div>
@@ -64,7 +66,7 @@ const UserHome = () => {
                 <div className="col-md-4 mb-4 position-relative">
                     <div className="cards shadow border-0">
                         <div className="cards-img-top">
-                      
+                        <img src={Cover05} alt="Banner5" className="w-100"/>
                         <Link to="/" className="image-overlay">Reviews</Link>
                         </div>
                     </div>
@@ -73,7 +75,7 @@ const UserHome = () => {
                 <div className="col-md-4 mb-4 position-relative">
                     <div className="cards shadow border-0">
                         <div className="cards-img-top">
-                            
+                            <img src={Cover06} alt="Banner6" className="w-100"/>
                             <Link to="/" className="image-overlay">Inquiries</Link>
                         </div>
                     </div>
